4411214e66e012a2c677e2db5e992788dfda16b5
KushagraSeth <60130610+KushagraSeth@users.noreply.github.com>
2026-03-14 17:54:15 +0530

    Update script.js

--- GLOBAL LOGISTICS & DISTRIBUTION LTD/js/script.js
+++ GLOBAL LOGISTICS & DISTRIBUTION LTD/js/script.js
@@ -1,61 +1,6 @@
-// const toggle = document.querySelector(".mobile-toggle");
-// const navLinks = document.querySelector(".nav-links");
-
-// if (toggle) {
-//   toggle.addEventListener("click", () => {
-//     navLinks.classList.toggle("active");
-//     toggle.classList.toggle("open");
-//   });
-// }
-
-// document.querySelectorAll("a[href^='#']").forEach(anchor => {
-//   anchor.addEventListener("click", function(e) {
-//     e.preventDefault();
-
-//     const target = document.querySelector(this.getAttribute("href"));
-
-//     if (target) {
-
-//       const headerOffset = 80;
-//       const elementPosition = target.offsetTop;
-//       const offsetPosition = elementPosition - headerOffset;
-
-//       window.scrollTo({
-//         top: offsetPosition,
-//         behavior: "smooth"
-//       });
-
-//       if (navLinks) {
-//         navLinks.classList.remove("active");
-//       }
-
-//     }
-//   });
-// });
-// const toggle2 = document.querySelector(".mobile-toggle");
-// const navMenu = document.querySelector(".nav-menu");
-
-// toggle2.addEventListener("click", () => {
-
-//   navMenu.classList.toggle2("active");
-
-//   if(navMenu.classList.contains("active")){
-//     toggle2.textContent = "✖";
-//   } else {
-//     toggle2.textContent = "☰";
-//   }
-
-// });
-// document.querySelectorAll(".nav-menu a").forEach(link=>{
-//   link.addEventListener("click",()=>{
-//     navMenu.classList.remove("active");
-//     toggle.textContent = "☰";
-//   });
-// });
-
 $(document).ready(function(){
 
-  const headerOffset = 60;
+  const headerOffset = 40;
 
   /* Mobile Menu Toggle */
 
